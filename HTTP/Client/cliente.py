3d943624e3873fbc912c636d34d7ae7ecefc9e70
Raphael - Raspeberry Pi <raphaelrdsoares@gmail.com>
2018-04-18 08:26:43 -0300

    alterando o cliente e dados.py

--- HTTP/Client/cliente.py
+++ HTTP/Client/cliente.py
@@ -7,20 +7,21 @@ import http.client, urllib.parse
 
 class Cliente():
 	_dados = Dados()
+	conn = ""
 	"""docstring for Cliente"""
 	def enviarPacote(self):
 		print("enviando pacote")
 		params = urllib.parse.urlencode({'@number': 12524, '@type': 'issue', '@action': 'show'})
 		headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-		conn.request("POST", "", params, headers)
-		response = conn.getresponse()
+		self.conn.request("POST", "", params, headers)
+		response = self.conn.getresponse()
 		print(response.status, response.reason)
 
 	def estabelecerConexao(self, address='localhost', port=8080):
 		print('Estabelecendo conexão...')
-		conn = http.client.HTTPConnection(address, port)
-		conn.request("HEAD", "/")
-		res = conn.getresponse()
+		self.conn = http.client.HTTPConnection(address, port)
+		self.conn.request("HEAD", "/")
+		res = self.conn.getresponse()
 		if res.status == 200:
 			print('conexão estebelecida!')
 		else:
@@ -28,5 +29,5 @@ class Cliente():
 
 
 cliente = Cliente()
-cliente.estabelecerConexao(port=8080)
+cliente.estabelecerConexao(address='169.254.173.134', port=8080)
 cliente.enviarPacote()
--- dados.py
+++ dados.py
@@ -1,12 +1,19 @@
 import random
+import sys
+
 
 class Dados():
 	"""docstring for Dados"""
 	def __init__(self):
 		super(Dados, self).__init__()
-		path = '/home/raphael/TCC/dados.txt'
+		print(sys.path)
+		path = sys.path[0] + '/dados.txt'
 		dataFile = open(path, 'r')
 		self.dados = dataFile.readlines()
 
 	def obterAleatorio(self):
 		return self.dados[random.randint(0,len(self.dados))]
+
+if __name__ == '__main__':
+    d = Dados()
+    print(d.obterAleatorio())
